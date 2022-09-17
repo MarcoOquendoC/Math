86d20a3b70ecc678e9847699d0e5c8c3f8a17155
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-17 01:36:13 -0500

    Add basic inline styling

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Quote = () => (
   <>
     <h2>Quote</h2>
-    <p>
+    <p
+      style={{
+        fontSize: '3rem',
+        fontWeight: '600',
+        marginBottom: '2rem',
+        lineHeight: '1em',
+        textAlign: 'center',
+      }}
+    >
       Mathematics is not about numbers, equations, computations, or
       algorithms: it is about understanding.
       <span> -- William Paul Thurston</span>
