aa9dcdc441a14fc3959a1266aec1a208758ef0fc
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-17 01:36:46 -0500

    Add classes and import styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './Home.module.css';
 
 const Home = () => (
   <div>
     <h2>Welcome to our page!</h2>
-    <p>
+    <p className={styles.item1}>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Animi cumque sit quaerat cum,ratione soluta! Eveniet, magnam
       eum ea excepturi et temporibus laborum at officia!
     </p>
-    <p>
+    <p className={styles.item2}>
       Lorem ipsum, dolor sit amet consectetur adipisicing elite.
       Animi cumque sit quaerat cum, ratione soluta! Eveniet, magnam
       eum ea excepturi et temporibus laborum at officia!
